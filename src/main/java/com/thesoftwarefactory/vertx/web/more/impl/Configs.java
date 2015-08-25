9e150b127f9d5802e665c65b261c5ba8cbe40294
Stephane Bastian <stephane.bastian@monpetitguide.com>
2015-08-25 15:18:17 +0200

    added  the helper class Configs to help wiht config files

---
+++ src/main/java/com/thesoftwarefactory/vertx/web/more/impl/Configs.java
@@ -0,0 +1,70 @@
+package com.thesoftwarefactory.vertx.web.more.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+
+public class Configs {
+
+	public final static Map<String, Object> extract(Map<String, Object> map, String prefixKey, boolean foldKeyNames) {
+		Map<String, Object> result = new HashMap<>();
+		if (map!=null && prefixKey!=null) {
+			for (String key: map.keySet()) {
+				if (key.startsWith(prefixKey)) {
+					Object value = map.get(key);
+					String targetKey = foldKeyNames ? key.substring(prefixKey.length()+1) : key;
+					result.put(targetKey, value);
+				}
+			}
+		}
+		return result;
+	}
+	
+	public final static JsonObject flatten(JsonObject input) {
+		JsonObject result = new JsonObject();
+		if (input!=null) {
+			flatten(null, input, result);
+		}
+		return result;
+	}
+
+	private final static void flatten(String prefix, JsonObject input, JsonObject output) {
+		if (input!=null && output!=null) {
+			for (String fieldName: input.fieldNames()) {
+				String targetFieldName = prefix!=null  ? prefix + "." + fieldName : fieldName;
+				Object value = input.getValue(fieldName);
+				if (value instanceof JsonObject) {
+					flatten(targetFieldName, (JsonObject) value, output);
+				}
+				else if (value instanceof JsonArray) {
+					flatten(targetFieldName, (JsonArray) value, output);
+				}
+				else {
+					output.put(targetFieldName, value);
+				}
+			}
+		}
+	}
+
+	private final static void flatten(String prefix, JsonArray input, JsonObject output) {
+		if (input!=null && output!=null) {
+			int index = 0;
+			for (Object value: input) {
+				String targetFieldName = prefix!=null  ? prefix + "." + index : "" +  index;
+				if (value instanceof JsonObject) {
+					flatten(targetFieldName, (JsonObject) value, output);
+				}
+				else if (value instanceof JsonArray) {
+					flatten(targetFieldName, (JsonArray) value, output);
+				}
+				else {
+					output.put(targetFieldName, value);
+				}
+				index++;
+			}
+		}
+	}
+
+}
---
+++ src/test/java/com/thesoftwarefactory/vertx/web/more/impl/TestConfigs.java
@@ -0,0 +1,84 @@
+package com.thesoftwarefactory.vertx.web.more.impl;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+import io.vertx.core.json.JsonObject;
+
+public class TestConfigs {
+
+	private JsonObject testJson() {
+		String json = "{"
+				+ "\"boolean\" : true,"
+				+ "\"int\" : 1,"
+				+ "\"string\" : \"string value\","
+				+ "\"array\" : ["
+					+ "\"string value 0\","
+					+ "{"
+							+ "\"boolean 1\" : false"
+					+ "},"
+					+ "["
+						+ "true,"
+						+ "\"string value 2.1\""
+					+ "]"
+				+ "],"
+				+ "\"object\" : {"
+					+ "\"boolean\" : true,"
+					+ "\"int\" : 2,"
+					+ "\"string\" : \"string value\","
+					+ "\"object\" : {"
+						+ "\"boolean\" : true,"
+						+ "\"int\" : 3,"
+						+ "\"string\" : \"string value\""
+					+ "},"
+					+ "\"array\" : ["
+						+ "true,"
+						+ "\"string value\""
+					+ "]"
+				+ "}"
+				+ "}";
+		return new JsonObject(json);
+	}
+
+	@Test
+	public void testExtract() {
+		JsonObject flattenJson = Configs.flatten(testJson());
+		Map<String, Object> map = flattenJson.getMap();
+		Map<String, Object> map2 = Configs.extract(map, "object.object", false);
+		assertTrue(map2.size()==3);
+		assertTrue(map2.get("object.object.boolean").equals(true));
+		assertTrue(map2.get("object.object.int").equals(3));
+		assertTrue(map2.get("object.object.string").equals("string value"));
+
+		Map<String, Object> map3 = Configs.extract(map, "object.object", true);
+		assertTrue(map3.size()==3);
+		assertTrue(map3.get("boolean").equals(true));
+		assertTrue(map3.get("int").equals(3));
+		assertTrue(map3.get("string").equals("string value"));
+	}
+	
+	@Test
+	public void testFlatten() {
+		JsonObject flattenJson = Configs.flatten(testJson());
+		assertTrue(flattenJson.size()==15);
+		assertTrue(flattenJson.getValue("boolean").equals(true));
+		assertTrue(flattenJson.getValue("int").equals(1));
+		assertTrue(flattenJson.getValue("string").equals("string value"));
+		assertTrue(flattenJson.getValue("array.0").equals("string value 0"));
+		assertTrue(flattenJson.getValue("array.1.boolean 1").equals(false));
+		assertTrue(flattenJson.getValue("array.2.0").equals(true));
+		assertTrue(flattenJson.getValue("array.2.1").equals("string value 2.1"));
+		assertTrue(flattenJson.getValue("object.boolean").equals(true));
+		assertTrue(flattenJson.getValue("object.int").equals(2));
+		assertTrue(flattenJson.getValue("object.string").equals("string value"));
+		assertTrue(flattenJson.getValue("object.object.boolean").equals(true));
+		assertTrue(flattenJson.getValue("object.object.int").equals(3));
+		assertTrue(flattenJson.getValue("object.object.string").equals("string value"));
+		assertTrue(flattenJson.getValue("object.array.0").equals(true));
+		assertTrue(flattenJson.getValue("object.array.1").equals("string value"));
+	}
+
+}
